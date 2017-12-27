70c114665ac4655ef6366215052a3f22348096da
JoshyPHP <s9e.dev@gmail.com>
2017-12-27 13:21:25 +0100

    Updated for Flarum Beta 8

--- bootstrap.php
+++ bootstrap.php
@@ -2,30 +2,21 @@
 
 /**
 * @package   s9e\autovideo
-* @copyright Copyright (c) 2016 The s9e Authors
+* @copyright Copyright (c) 2016-2017 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\Flarum\Autovideo;
 
-use Flarum\Event\ConfigureFormatter;
+use Flarum\Formatter\Event\Configuring;
 use Illuminate\Events\Dispatcher;
 
 function subscribe(Dispatcher $events)
 {
 	$events->listen(
-		ConfigureFormatter::class,
-		function (ConfigureFormatter $event)
+		Configuring::class,
+		function (Configuring $event)
 		{
-			if (!isset($event->configurator->tags['VIDEO']))
-			{
-				$tag = $event->configurator->tags->add('VIDEO');
-				$tag->attributes->add('src')->filterChain->append('#url');
-				$tag->template = '<video controls="" src="{@src}"/>';
-			}
-			$event->configurator->Preg->match(
-				'#\\b(?<src>https?://[-.\\w]+/[-./\\w]+\\.(?:mp4|ogg|webm))(?!\\S)#i',
-				'VIDEO'
-			);
+			$event->configurator->Autovideo;
 		}
 	);
 };
